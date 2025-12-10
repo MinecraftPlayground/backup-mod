73e8abe8512b7da5024da954d324a39ce5e48c3b
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-12-10 22:56:39 +0100

    feat: init

---
+++ src/main/java/dev/loat/Backup.java
@@ -0,0 +1,21 @@
+package dev.loat;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Backup implements ModInitializer {
+  public static final String MOD_ID = "backup";
+
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+  @Override
+  public void onInitialize() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
+
+    LOGGER.info("Hello Fabric world!");
+  }
+}
