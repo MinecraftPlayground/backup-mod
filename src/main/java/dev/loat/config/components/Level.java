fe2f4257d41ee75b503d5565c4c2f5d1919a1ae9
Mqx <62719703+Mqxx@users.noreply.github.com>
2025-12-19 23:28:35 +0100

    Delete Level.java

--- src/main/java/dev/loat/config/components/Level.java
+++
@@ -1,8 +0,0 @@
-package dev.loat.config.components;
-
-public class Level {
-    public static String DEBUG = "DEBUG";
-    public static String INFO = "INFO";
-    public static String WARN = "WARN";
-    public static String ERROR = "ERROR";
-}
